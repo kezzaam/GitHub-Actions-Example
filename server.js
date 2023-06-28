440f278e87e107f8073cd31e7458f1f7a5fba315
Keren Elizabeth <kezzaam@hotmail.com>
2023-06-28 19:13:30 +1200

    Commit 2

---
+++ server.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 3000; // changed from 8080
+const HOST = '127.0.0.1'; // changed from 'localhost'
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(PORT, HOST, () => {
+  console.log(`Running on http://${HOST}:${PORT}`);
+});
